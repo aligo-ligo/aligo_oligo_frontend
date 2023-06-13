f61287fde40d7c088b06ec4c1242dc60060845a7
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-06-13 19:29:00 +0900

    Design : 랜딩 페이지 기본 UI 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
-
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl">초기 세팅!!~~</h1>
-    </div>
-  )
+	return (
+		<section className="flex flex-col items-center justify-center h-screen px-6 py-10 overflow-hidden">
+			<div className="flex flex-col items-center justify-center">
+				<h1 className="text-4xl font-bold mb-4">Aligo Oligo</h1>
+				<h2 className="text-2xl font-medium text-gray mb-12">
+					공유하여 목표를 달성해보세요!
+				</h2>
+			</div>
+			<p className="text-2xl font-medium text-gray mb-24">
+				현재 187,054개의 티켓을 성취했어요!
+			</p>
+
+			<div className="flex flex-col items-center justify-center w-full ">
+				<Link
+					href="/signin"
+					className="w-full h-full flex items-center justify-center"
+				>
+					<button className="w-9/12 h-20 bg-main rounded-md text-xl text-white mx-auto mb-12">
+						로그인
+					</button>
+				</Link>
+
+				<button className="text-xl text-main">
+					<Link href="/signup">회원가입 하기</Link>
+				</button>
+			</div>
+		</section>
+	);
 }
